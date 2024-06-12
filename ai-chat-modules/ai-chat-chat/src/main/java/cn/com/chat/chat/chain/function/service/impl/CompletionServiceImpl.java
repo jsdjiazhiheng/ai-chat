70a207a640f7603e4d343fbb8ba67a34abca8814
JiaZH <1412148237@qq.com>
2024-06-12 11:26:49 +0800

    refactor(chat): 更新CompletionServiceImpl，通过配置来控制任务执行模型

--- ai-chat-modules/ai-chat-chat/src/main/java/cn/com/chat/chat/chain/function/service/impl/CompletionServiceImpl.java
+++ ai-chat-modules/ai-chat-chat/src/main/java/cn/com/chat/chat/chain/function/service/impl/CompletionServiceImpl.java
@@ -2,12 +2,14 @@ package cn.com.chat.chat.chain.function.service.impl;
 
 import cn.com.chat.chat.chain.response.base.text.TextResult;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import cn.com.chat.chat.chain.enums.TextChatType;
 import cn.com.chat.chat.chain.function.service.ICompletionService;
 import cn.com.chat.chat.chain.generation.function.FunctionChatService;
 import cn.com.chat.chat.chain.generation.text.TextChatService;
 import cn.com.chat.common.core.utils.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 /**
@@ -18,25 +20,30 @@ import org.springframework.stereotype.Service;
  */
 @Service
 @Slf4j
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class CompletionServiceImpl implements ICompletionService {
 
     private final TextChatService textChatService;
     private final FunctionChatService functionChatService;
+    @Value("ai.completion.model")
+    private String model;
 
+    private TextChatType getTextChatType() {
+        return TextChatType.getByName(model);
+    }
 
     @Override
     public Boolean functionSearch(String content) {
-        String fullContent = "判断以下文本是否需要联网查询，你只能回答1个字：是或者否：" + content;
-        TextResult result = textChatService.blockCompletion(TextChatType.KIMI, null, null, fullContent, Boolean.FALSE);
+        String fullContent = "判断以下文本是否需要联网查询，强制回答1个字：是或否：\n\n" + content;
+        TextResult result = textChatService.blockCompletion(getTextChatType(), null, null, fullContent, Boolean.FALSE);
         log.info("判断是否需要联网：{}", result.getContent());
         return result.getContent().contains("是");
     }
 
     @Override
     public Boolean functionSecurity(String content) {
-        String fullContent = "判断以下文本是否包含政治、暴力、色情、毒品、恐怖、歧视等敏感内容，你只能回答2个字：安全或者危险：" + content;
-        TextResult result = textChatService.blockCompletion(TextChatType.KIMI, null, null, fullContent, Boolean.FALSE);
+        String fullContent = "判断以下文本是否包含政治、暴力、色情、毒品、恐怖、歧视等敏感内容，强制回答2个字：安全或危险：\n\n" + content;
+        TextResult result = textChatService.blockCompletion(getTextChatType(), null, null, fullContent, Boolean.FALSE);
         log.info("判断输入文本是否安全：{}", result.getContent());
         return result.getContent().contains("安全");
     }
@@ -62,8 +69,8 @@ public class CompletionServiceImpl implements ICompletionService {
               纠正后的文本："星空下的城堡，古老的传说，神秘的宝藏"
               生成的图像提示词："一个星空下的城堡，城堡周围环绕着古老的传说和神秘的宝藏。"
             """;
-        String fullContent = "将以下内容按照顺序理解输入词语意思，并保证正确性，不能出现常识性错误，并生成最终结果的文生图提示词，你只能回答最终的文生图提示词结果：" + content;
-        TextResult result = textChatService.blockCompletion(TextChatType.KIMI, system, null, fullContent, Boolean.FALSE);
+        String fullContent = "将以下内容按照顺序理解输入词语意思，并保证正确性，不能出现常识性错误，并生成最终结果的文生图提示词，强制回答最终的文生图提示词结果，不能重复问题的内容，不能出现除提示词外的其它内容：\n\n" + content;
+        TextResult result = textChatService.blockCompletion(getTextChatType(), system, null, fullContent, Boolean.FALSE);
         String resultContent = result.getContent();
         log.info("理解图片输入词，最终结果为：{}", resultContent);
         if(resultContent.contains("：")) {
